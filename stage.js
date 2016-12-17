1e397f9fe3cdcafbccf93089476d01ecb7ad3967
joelc1337 <joelcolon1337@gmail.com>
2016-12-17 13:07:59 -0500

    script seperation

--- stage.js
+++ stage.js
@@ -1,25 +1,33 @@
-// Stage scripts
-
-function game(){
-	newStage(stage1)
-
-}
-
-function newStage(stage){
-	// Changes text in question div to match the "stage" question
-	$("#question").text(stage1.question);
-
-
-	// changes text in answer choice divs to match the answer choices for the question
-	$("#answerA").text("A." + stage1.answerA);
-	$("#answerB").text("B." + stage1.answerB);
-	$("#answerC").text("C." +stage1.answerC);
-}
+var stages = [stage1,stage2,stage3,stage4]
 
 var stage1 = {
 	question: "",
 
 	answerA: "", 
+	answerB: "",
+	answerC: "",
+}
+
+var stage2 = {
+	question: "",
+
+	answerA: "",
+	answerB: "",
+	answerC: "",
+}
+
+var stage3 = {
+	question: "",
+
+	answerA: "",
+	answerB: "",
+	answerC: "",
+}
+
+var stage4 = {
+	question: "",
+
+	answerA: "",
 	answerB: "",
 	answerC: "",
 }
