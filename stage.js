13ce7d5ffb22be54ccd07c796237882bcd7f32e5
joelc1337 <joelcolon1337@gmail.com>
2016-12-17 13:44:15 -0500

    object changes

--- stage.js
+++ stage.js
@@ -3,31 +3,67 @@ var stages = [stage1,stage2,stage3,stage4]
 var stage1 = {
 	question: "",
 
-	answerA: "", 
-	answerB: "",
-	answerC: "",
+	answerA = {
+		text: "",
+		points: ,
+	}
+	answerB = {
+		text: "",
+		points: ,
+	}
+	answerC = {
+		text: "",
+		points: ,
+	}
 }
 
 var stage2 = {
 	question: "",
 
-	answerA: "",
-	answerB: "",
-	answerC: "",
+	answerA = {
+		text: "",
+		points: ,
+	}
+	answerB = {
+		text: "",
+		points: ,
+	}
+	answerC = {
+		text: "",
+		points: ,
+	}
 }
 
 var stage3 = {
 	question: "",
 
-	answerA: "",
-	answerB: "",
-	answerC: "",
+	answerA = {
+		text: "",
+		points: ,
+	}
+	answerB = {
+		text: "",
+		points: ,
+	}
+	answerC = {
+		text: "",
+		points: ,
+	}
 }
 
 var stage4 = {
 	question: "",
 
-	answerA: "",
-	answerB: "",
-	answerC: "",
+	answerA = {
+		text: "",
+		points: ,
+	}
+	answerB = {
+		text: "",
+		points: ,
+	}
+	answerC = {
+		text: "",
+		points: ,
+	}
 }
